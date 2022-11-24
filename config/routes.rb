0fb8fbf22ab552042e846bd44f95a32dd9bdf768
Guangxin Yu <1572250276@qq.com>
2022-11-24 10:27:29 +0800

    finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, default: { format: :json } do
+    namespace :v1 do
+      resources :stories, only: [:index]
+    end
+  end
 end
