ddad61a35d8df533bd3b1804ea337b9fa0a303da
Guangxin Yu <1572250276@qq.com>
2022-11-24 14:05:09 +0800

    finished action create for storiesController

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -8,9 +8,22 @@ class Api::V1::StoriesController < Api::V1::BaseController
 
   def show; end
 
+  def create
+    @story = Story.new(story_params);
+    if @story.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_story
     @story = Story.find(params[:id])
+  end
+
+  def story_params
+    params.require(:story).permit(:title, :content, :picture_url)
   end
 end
